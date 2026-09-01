807e7d8bcc1e58c377eb14742d55bfe6b6aa0023
Nyaruki_ <hahaha.harugakita45@gmail.com>
2026-09-01 19:54:01 +0900

    APIBASEの更新

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 // API クライアント共通
-const API_BASE = 'https://seiriken-api.soheki-numberedticket.workers.dev';
+export const API_BASE = window.API_BASE ?? \x27\x27;
 
 export async function apiFetch(path, options = {}) {
   const token = localStorage.getItem('seiriken_token');
